63c8ce3b1e9f07fb823c60b315f75ac78d926027
nguyenluc25 <nguyenluc1234012@gmail.com>
2025-03-01 08:46:32 +0700

    thuc hanh design pattern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import singleton.BillPughSingleton;
+import singleton.EagerInitializedSingleton;
+import singleton.LazyInitializedSingleton;
+import singleton.StaticBlockSingleton;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EagerInitializedSingleton eagerInitializedSingleton = EagerInitializedSingleton.getInstance();
+        System.out.println(eagerInitializedSingleton.hashCode());
+
+        EagerInitializedSingleton eagerInitializedSingleton1 = EagerInitializedSingleton.getInstance();
+        System.out.println(eagerInitializedSingleton1.hashCode());
+
+        System.out.println("-------------------------------");
+
+        StaticBlockSingleton staticBlockSingleton = StaticBlockSingleton.getInstance();
+        System.out.println(staticBlockSingleton.hashCode());
+
+        StaticBlockSingleton staticBlockSingleton1 = StaticBlockSingleton.getInstance();
+        System.out.println(staticBlockSingleton1.hashCode());
+
+        System.out.println("-------------------------------");
+
+        LazyInitializedSingleton lazyInitializedSingleton = LazyInitializedSingleton.getInstance();
+        System.out.println(lazyInitializedSingleton.hashCode());
+
+        LazyInitializedSingleton lazyInitializedSingleton1 = LazyInitializedSingleton.getInstance();
+        System.out.println(lazyInitializedSingleton1.hashCode());
+
+        System.out.println("-------------------------------");
+
+        
+
+
     }
 }
